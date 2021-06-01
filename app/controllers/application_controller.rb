effa976ce3dadebc03c7524978d6fd3c93f96f5c
Jung Lee <lee.jung.s95@gmail.com>
2021-06-01 07:38:58 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,6 +37,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
+    user = User.find_by(username: params[:username], password: params[:password])
+    if user.save
+      session[:user_id] = user.id
+      redirect "/account"
+    else
+      redirect "failure"
+    end
     ##your code here
   end
 
